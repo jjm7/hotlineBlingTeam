e10295f9cdc56c51932bd4becdc0b577f24d220a
Joshua Michalenko <joshmichalenko@yahoo.com>
2015-11-22 01:45:01 -0600

    added some songs

--- josh/syncronizeSongs.py
+++ josh/syncronizeSongs.py
@@ -1,12 +1,20 @@
 import json
 import requests
 import urllib
-
+import echonest.remix.audio as audio
+from pyechonest import config
 
 API_KEY = 'RZ1JEQ5GYJZA2J1I7'
 BASE = 'http://developer.echonest.com/api/v4/'
-ARTIST = 'kanye%20west'
-SONG = 'all%20of%20the%20lights'
+ARTIST = 'drake'
+SONG = 'hotline%20bling'
+
+FILE_NAME = 'Drake - Hotline Bling.mp3'
+
+config.ECHO_NEST_API_KEY=API_KEY
+
+#ARTIST = 'kanye%20west'
+#SONG = 'all%20of%20the%20lights'
 
 ## Lets do some stupid html requests to get song data
 r = urllib.urlopen(BASE +'song/search?api_key='+API_KEY+ '&artist=' + ARTIST + '&title='+SONG)
@@ -17,6 +25,17 @@ songTempo =u['response']['songs'][0]['audio_summary']['tempo']
 
 #Finnaly have the tempo of the song
 
-print songTempo
+print u
+
+# Easy around wrapper mp3 decoding and Echo Nest analysis
+audio_file = audio.LocalAudioFile(FILE_NAME)
+
+# You can manipulate the beats in a song as a native python list
+beats = audio_file.analysis.beats
+beats.reverse()
+
+# And render the list as a new audio file!
+audio.getpieces(audio_file, beats).encode("backwards.mp3")
+            
 
 
